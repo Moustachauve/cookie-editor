01f070ff8c6c9e086293a867ed2f5548a0a0d673
Moustachauve <Moustachauve@users.noreply.github.com>
2018-08-22 10:39:52 -0400

    Add event tracking

--- site/script/stickyHeader.js
+++ site/script/stickyHeader.js
@@ -6,9 +6,8 @@
 
         // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
         function onScroll() {
-            console.log('yo');
             if (window.pageYOffset >= header.offsetHeight) {
-                navbar.classList.add("sticky")
+                navbar.classList.add("sticky");
             } else {
                 navbar.classList.remove("sticky");
             }
