9da2ee2e193061d6bfc9bfeb341323a12a29b9bd
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2018-08-21 21:15:50 -0400

    Fix issues with navigation position

--- site/script/stickyHeader.js
+++ site/script/stickyHeader.js
@@ -1,13 +1,27 @@
-var navbar = document.getElementById("main-nav");
-var sticky = navbar.offsetTop;
+(function(){
+    document.addEventListener('DOMContentLoaded', function () {
+        var header = document.getElementById("main-header");
+        var navbar = document.getElementById("main-nav");
+        var resizeTimeout;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function onScroll() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-window.onscroll = onScroll;
-onScroll();
+        // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+        function onScroll() {
+            console.log('yo');
+            if (window.pageYOffset >= header.offsetHeight) {
+                navbar.classList.add("sticky")
+            } else {
+                navbar.classList.remove("sticky");
+            }
+        }
+
+        window.onscroll = onScroll;
+        window.onresize = function () {
+            if (resizeTimeout) {
+                clearTimeout(resizeTimeout);
+            }
+
+            resizeTimeout = setTimeout(onScroll, 50);
+        };
+        onScroll();
+    });
+})();
