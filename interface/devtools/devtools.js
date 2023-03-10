7a6b1103ab43e4d9909951f46fc85171db059c0d
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2023-03-10 02:18:11 -0500

    Add Netscape format cookie export  #17

--- interface/devtools/devtools.js
+++ interface/devtools/devtools.js
@@ -1,6 +1,6 @@
 chrome.devtools.panels.create("Cookie Editor",
     "/icons/cookie-filled-small.svg",
-    "/interface/popup-android/cookie-list.html",
+    "/interface/devtools/cookie-list.html",
     function(panel) {
 
 });
