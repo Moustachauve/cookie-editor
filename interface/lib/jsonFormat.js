cf7f6dececbbc76920b363f658e8839b592cd67a
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2023-08-29 21:58:47 -0400

    Refactor JSON and Netscape import/export code

---
+++ interface/lib/jsonFormat.js
@@ -0,0 +1,34 @@
+/**
+ * This class is responsible for parsing and formatting cookies to the
+ * JSON format.
+ */
+export class JsonFormat {
+  /**
+   * Parses a string of cookie in the JSON format to a cookie object.
+   * @param {string} cookieString Cookies in the JSON format.
+   * @return {object} List of Cookies.
+   */
+  static parse(cookieString) {
+    return JSON.parse(cookieString);
+  }
+
+  /**
+   * Formats a list of cookies into a JSON formatted string.
+   * @param {object} cookies Cookies to format.
+   * @return {string} JSON formatted cookie string.
+   */
+  static format(cookies) {
+    const exportedCookies = [];
+    for (const cookieId in cookies) {
+      if (Object.prototype.hasOwnProperty.call(cookies, cookieId)) {
+        const exportedCookie = cookies[cookieId].cookie;
+        exportedCookie.storeId = null;
+        if (exportedCookie.sameSite === 'unspecified') {
+          exportedCookie.sameSite = null;
+        }
+        exportedCookies.push(exportedCookie);
+      }
+    }
+    return JSON.stringify(exportedCookies, null, 4);
+  }
+}
