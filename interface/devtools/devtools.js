57a40c728352ffe8bceaa5a61c48683fe37f801e
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2023-09-10 01:29:28 -0400

    Add feature to toggle on or off the devtools.

--- interface/devtools/devtools.js
+++ interface/devtools/devtools.js
@@ -1,6 +1,48 @@
-chrome.devtools.panels.create(
-  'Cookie Editor',
-  '/icons/cookie-filled-small.svg',
-  '/interface/devtools/cookie-list.html',
-  function (panel) {},
-);
+import { BrowserDetector } from '../lib/browserDetector.js';
+import { GenericStorageHandler } from '../lib/genericStorageHandler.js';
+import { OptionsHandler } from '../lib/optionsHandler.js';
+
+(async function () {
+  ('use strict');
+  const browserDetector = new BrowserDetector();
+  const storageHandler = new GenericStorageHandler(browserDetector);
+  const optionHandler = new OptionsHandler(browserDetector, storageHandler);
+
+  await optionHandler.loadOptions();
+  optionHandler.on('optionsChanged', onOptionsChanged);
+  handleDevtools();
+
+  /**
+   * Creates the devtools panel.
+   */
+  function createDevtools() {
+    browserDetector
+      .getApi()
+      .devtools.panels.create(
+        'Cookie Editor',
+        '/icons/cookie-filled-small.svg',
+        '/interface/devtools/cookie-list.html',
+        function (panel) {},
+      );
+  }
+
+  /**
+   * Shows or hides the devtools depending on the options.
+   */
+  function handleDevtools() {
+    if (optionHandler.getDevtoolsEnabled()) {
+      createDevtools();
+    }
+  }
+
+  /**
+   * Handles the changes required to the devtools when the options are changed
+   * by an external source.
+   * @param {Option} oldOptions the options before changes.
+   */
+  function onOptionsChanged(oldOptions) {
+    if (oldOptions.devtoolsEnabled != optionHandler.getDevtoolsEnabled()) {
+      handleDevtools();
+    }
+  }
+})();
